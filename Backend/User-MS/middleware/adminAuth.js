d0df11542067b9fa233e7c9223507c715d7cd6a3
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-02-04 16:24:51 +0100

    Add user Middlewares  & Validators

---
+++ Backend/User-MS/middleware/adminAuth.js
@@ -0,0 +1,22 @@
+exports.userMiddleware = (req, res, next) => {
+    if (req.user.role !== "user") {
+      return res.status(400).json({ message: "User access denied" });
+    }
+    next();
+  };
+  
+  exports.adminMiddleware = (req, res, next) => {
+    if (req.user.role !== "admin") {
+      if (req.user.role !== "super-admin") {
+        return res.status(400).json({ message: "Admin access denied" });
+      }
+    }
+    next();
+  };
+  
+  exports.superAdminMiddleware = (req, res, next) => {
+    if (req.user.role !== "super-admin") {
+      return res.status(200).json({ message: "Super Admin access denied" });
+    }
+    next();
+  };  
