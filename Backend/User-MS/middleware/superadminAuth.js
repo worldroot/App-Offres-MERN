20104ce1c3fe137031bbb4a5913eb744b8ca2605
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-02-06 12:16:49 +0100

    midd auth for admin & super

--- Backend/User-MS/middleware/superadminAuth.js
+++ Backend/User-MS/middleware/superadminAuth.js
@@ -7,13 +7,13 @@ module.exports = async function (req, res, next) {
             _id: req.user.id
         })
 
-        if (req.user.role !== "admin") {
-          return res.status(403).json({
-              error: 'Admin resources access denied'
-          })
+        if (req.user.role !== "super-admin") {
+            return res.status(403).json({
+                error: 'Super Admin resources access denied'
+            })
         }
+
         next()
-        
     } catch (error) {
         console.log(error)
         res.status(500).send('Server Error')
