dd870de8c3ed0b7fa74228b05603575a20bdd27e
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-02-15 22:37:36 +0100

    Fixed token to Local Storage

--- Frontend/admin/src/helpers/authToken.js
+++ Frontend/admin/src/helpers/authToken.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const setAuthToken = token => {
     if (token){
-        axios.defaults.headers.common['authorization'] = token;
+        axios.defaults.headers.Authorization =  token ? `Bearer ${token}` : '';
+        //axios.defaults.headers.Authorization= token;
     } else (
-        delete axios.defaults.headers.common['authorization']
+        delete axios.defaults.headers.Authorization
     )
 }
 
