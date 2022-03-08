8ad088922a6af797eb4357b747b5a20d8f24bd96
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-03-08 17:34:16 +0100

    update docker compose

--- Frontend/client/src/components/Footers/AuthFooter.js
+++ Frontend/client/src/components/Footers/AuthFooter.js
@@ -7,7 +7,7 @@ import { NavItem, NavLink, Nav, Container, Row, Col } from "reactstrap";
 const Login = () => {
   return (
     <>
-      <footer className="bg-darkgrey py-3">
+      <footer className="bg-darkgrey py-3 fixed-bottom">
         <Container>
           <Row className="align-items-center justify-content-xl-between">
             <Col xl="6">
