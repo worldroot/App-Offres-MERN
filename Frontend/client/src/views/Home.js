574b78e4dcce745ce7a3fd99ef6d9d0f293ff29b
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-06-22 16:19:26 +0100

    Add frontend client design

--- Frontend/client/src/views/Home.js
+++ Frontend/client/src/views/Home.js
@@ -1,25 +1,35 @@
-import { Row, Col, Container, UncontrolledCarousel } from "reactstrap";
+import { Row, Col, Container, UncontrolledCarousel, Button } from "reactstrap";
 
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import AuthNavbar from "components/Navbars/AuthNavbar.js";
 import AuthFooter from "components/Footers/AuthFooter.js";
 
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import { connect, useDispatch } from "react-redux";
 
 import { refreshJwt } from "redux/auth/authActions";
+import { allPub } from "redux/offres/offreActions";
 import decode from "jwt-decode";
-const ooredoo = require("../assets/img/oored.png");
 
-const Home = () => {
-  const userExist = localStorage.getItem("user");
-  const [user] = useState(() => {
-    const saved = localStorage.getItem("user");
-    const initialValue = JSON.parse(saved);
-    return initialValue || "";
-  });
+const bg1 = require("../assets/img/bg-offres-01.png");
+const bg2 = require("../assets/img/bg-offres-04.png");
+const bg3 = require("../assets/img/bg-offres-05.png");
+
+const add = require("../assets/img/undraw_Add_files_re_v09g.png");
+const select = require("../assets/img/undraw_Selecting_re_5ff6.png");
+const final = require("../assets/img/undraw_Agreement_re_d4dv.png");
+const dep = require("../assets/img/undraw_Folder_files_re_2cbm.png");
+
+const Home = ({...props}) => {
+
+  useEffect(() => {
+    props.AllPub();
+    props.AllCat();
+  }, []);
+
   const dispatch = useDispatch();
+  const history = useHistory();
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
@@ -37,56 +47,120 @@ const Home = () => {
     }
   }, []);
 
-  const items = [
-    {
-      src: ooredoo,
+  const ImgStyle = {
+    width: 400,
+    height: 300,
+  };
 
-    },
-    {
-      src: ooredoo,
-
-    },
-
-  ];
+  const items = [{ src: bg1 }, { src: bg2 }, { src: bg3 }];
 
   return (
     <>
       <div className="main-content">
         <AuthNavbar />
 
-        {/* Content */}
-        {!userExist && (
-          <>
-            <Container className="mt-4 mb-4 py-4 w-100vh h-100vh">
-              <Row className="justify-content-center">
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 1.5 }}
-                >
-                  <h1 className="text-center text-red">Acceuil</h1>
-                </motion.div>
-              </Row>
-            </Container>
-          </>
-        )}
+        <Container className="justify-content-center bg-white text-center w-75">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+          >
+            <UncontrolledCarousel items={items} />
+          </motion.div>
+        </Container>
 
-        {userExist && (
-          <>
-            <Container className="mt-4 mb-4 py-4 w-100vh h-100vh">
-              <Row className="justify-content-center">
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 1.5 }}
+        <Container className="mt-4">
+          <h1 className="text-dark text-center m-4 border-bottom border-danger">
+            SOLUTION
+          </h1>
+          <motion.div
+            initial={{ x: "400vh" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1.5 }}
+          >
+            <Row className="m-2">
+              <Col>
+                <h1 className="text-dark">Publication des appels d'offres</h1>
+                <h2 className="text-gray">
+                  Chaque offre publiée contient tous les détails nécessaires
+                  (Prix, date de début et de fin, description...).
+                </h2>
+                <Button
+                  className="my-4 btn-outline-dark"
+                  onClick={() => history.push("/published-offres")}
+                  type="submit"
                 >
-                  <h1 className="text-center text-red">Bienvenue</h1>
-                </motion.div>
-              </Row>
-            </Container>
-            {/* <UncontrolledCarousel items={items} /> */}
-          </>
-        )}
+                  Les appels doffres publiés
+                </Button>
+              </Col>
+              <Col>
+                <img className="w-100 h-100" src={add} alt="" />
+              </Col>
+            </Row>
+          </motion.div>
+
+          <motion.div
+            initial={{ x: "-400vh" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1.5 }}
+          >
+            <Row className="m-2">
+              <Col>
+                <img className="w-100 h-100" src={select} alt="" />
+              </Col>
+              <Col>
+                <h1 className="text-dark">Soumettre une demande</h1>
+                <h2 className="text-gray">
+                  Après avoir vérifier votre compte vous pouvez soumettre une
+                  demande de l'offre choisis en ajoutant un prix.
+                </h2>
+              </Col>
+            </Row>
+          </motion.div>
+
+          <motion.div
+            initial={{ x: "-400vh" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1.5 }}
+          >
+            <Row className="m-2">
+              <Col>
+                <h1 className="text-dark">Depouillement</h1>
+                <h2 className="text-gray">
+                  Après la fin de l'offre, le dépouillement des soumissions
+                  commence pour avoir le meilleur prix propose.
+                </h2>
+              </Col>
+              <Col>
+                <img className="w-100 h-100" src={dep} alt="" />
+              </Col>
+            </Row>
+          </motion.div>
+
+          <motion.div
+            initial={{ x: "400vh" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1.5 }}
+          >
+            <Row className="m-2">
+              <Col>
+                <img className="w-100 h-100" src={final} alt="" />
+              </Col>
+              <Col>
+                <h1 className="text-dark">Résultats</h1>
+                <h2 className="text-gray">
+                  Si votre soumission a eu le meilleur prix proposé vous serez
+                  contacté par l'un des agents d'Ooredoo.
+                </h2>
+              </Col>
+            </Row>
+          </motion.div>
+
+          <h1 className="text-dark text-center m-4 border-bottom border-danger">
+            LES APPEL D'OFFRES
+          </h1>
+          
+        </Container>
 
         <AuthFooter />
       </div>
@@ -94,9 +168,16 @@ const Home = () => {
   );
 };
 
-const mapToStateProps = (state) => ({
+
+
+const mapStateToProps = (state) => ({
+  Listpub: state.offres.offres,
   isAuth: state.auth.isAuthenticated,
-  user: state.auth.user,
+  isLoading: state.offres.loading,
 });
 
-export default connect(mapToStateProps)(Home);
+const mapActionToProps = {
+  AllPub: allPub
+};
+
+export default connect(mapStateToProps, mapActionToProps)(Home);
